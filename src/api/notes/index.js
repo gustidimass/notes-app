597c2de321dddf53f04a13aeca5bbcf859d9be76
gustidimass <gustidimas11@gmail.com>
2023-04-06 14:36:18 +0800

    edit index.js

--- src/api/notes/index.js
+++ src/api/notes/index.js
@@ -1,5 +1,32 @@
 const NotesHandler = require("./handler");
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+
+const init = async () => {
+    const notesService = new NotesService();
+
+    const server = Hapi.server({
+        port: 5000,
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+    await server.register({
+        plugin: notes,
+        option: {
+            service: notesService,
+        },
+    });
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
 
 module.exports = {
     name: 'notes',
