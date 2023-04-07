e1d75879b7ab449a8b65448542dd88b1ee62b0c4
gustidimass <gustidimas11@gmail.com>
2023-04-07 14:54:21 +0800

    restructure with hapi plugin

--- src/api/notes/index.js
+++ src/api/notes/index.js
@@ -1,38 +1,37 @@
-const NotesHandler = require("./handler");
+const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
-
+ 
 const init = async () => {
-    const notesService = new NotesService();
-
-    const server = Hapi.server({
-        port: 5000,
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-        routes: {
-            cors: {
-                origin: ['*'],
-            },
-        },
-    });
-
-    await server.register({
-        plugin: notes,
-        option: {
-            service: notesService,
-        },
-    });
-
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+  const notesService = new NotesService();
+  const server = Hapi.server({
+    port: 7000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+ 
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
+ 
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
-
+ 
 init();
 
 module.exports = {
     name: 'notes',
     version: '1.0.0',
-    register: async (server, { service}) => {
-        const notesHandler = new NotesHandler(service);
-        server.route(routes(notesHandler));
+    register: async (server, { service }) => {
+      const notesHandler = new NotesHandler(service);
+      server.route(routes(notesHandler));
     },
-}
+  };
